9a7d0cf6d1e28468e822334b254e27983130011b
Akshay-katiyar16 <akshaykatiyar1666@gmail.com>
2026-01-09 15:52:16 +0530

    fix: update NotFound page title for clarity

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -2,7 +2,7 @@
 const NotFound = () => {
   return (
     <div className="min-h-screen flex justify-center items-center bg-gray-300 text-3xl text">
-      <h1>! Page Not Found</h1>
+      <h1>404 !Error Page Not Found!</h1>
     </div>
   )
 }
